f6b067b82231e48bf56bdbf25c8687c0b352f784
matheus <matheusbritodasilva@gmail.com>
2019-09-23 10:36:19 -0300

    Avatar do usuário

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import multer from 'multer';
 import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -16,6 +17,4 @@ routes.put('/users', UserController.update);
 
 export default routes;
 
-routes.post('/files', upload.single('file'), (req, res) => {
-    return res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), FileController.store);
