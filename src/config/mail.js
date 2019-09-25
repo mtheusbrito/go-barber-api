a6d74a71c4890a4df80fbcc8ca42d1cefe8bf620
matheus <matheusbritodasilva@gmail.com>
2019-09-25 13:28:15 -0300

    Configurando Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,14 @@
+import { Session } from 'inspector';
+
+export default {
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    secure: false,
+    auth: {
+        user: '8c1e81a65eec3e',
+        pass: '0dbbb33c35a9c8',
+    },
+    default: {
+        from: 'Matheus Brito <noreply@gobarber.com>',
+    },
+};
