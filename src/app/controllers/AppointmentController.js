b59dd91557908eb3c68b1281ad3e63a9e5bf84aa
matheus <matheusbritodasilva@gmail.com>
2019-09-25 16:21:17 -0300

    Configurando templates de e-mail

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,6 +1,8 @@
 import * as Yup from 'yup';
+
 import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt-BR';
+
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
@@ -108,6 +110,11 @@ class AppointmentController {
                     as: 'provider',
                     attributes: ['name', 'email'],
                 },
+                {
+                    model: User,
+                    as: 'user',
+                    attributes: ['name'],
+                },
             ],
         });
 
@@ -128,7 +135,18 @@ class AppointmentController {
         await Mail.sendMail({
             to: `${appointment.provider.name} <${appointment.provider.email}>`,
             subject: 'Agendamento cancelado',
-            text: 'Voce tem um novo cancelamento',
+            template: 'cancelation',
+            context: {
+                provider: appointment.provider.name,
+                user: appointment.user.name,
+                date: format(
+                    appointment.date,
+                    "'dia' dd 'de' MMMM', às 'H:mm'h'",
+                    {
+                        locale: pt,
+                    }
+                ),
+            },
         });
         return res.json(appointment);
     }
--- src/lib/Mail.js
+++ src/lib/Mail.js
@@ -1,16 +1,37 @@
 import nodemailer from 'nodemailer';
+import exphbs from 'express-handlebars';
+import nodemailerghbs from 'nodemailer-express-handlebars';
+import { resolve } from 'path';
 import mailConfig from '../config/mail';
 
 class Mail {
     constructor() {
         const { host, port, secure, auth } = mailConfig;
 
-        this.transporter = nodemailer.createTestAccount({
+        this.transporter = nodemailer.createTransport({
             host,
             port,
             secure,
             auth: auth.user ? auth : null,
         });
+        this.configureTemplates();
+    }
+
+    configureTemplates() {
+        const viewPath = resolve(__dirname, '..', 'app', 'views', 'emails');
+        this.transporter.use(
+            'compile',
+            nodemailerghbs({
+                viewEngine: exphbs.create({
+                    layoutsDir: resolve(viewPath, 'layouts'),
+                    partialsDir: resolve(viewPath, 'partials'),
+                    defaultLayout: 'default',
+                    extname: '.hbs',
+                }),
+                viewPath,
+                extName: '.hbs',
+            })
+        );
     }
 
     sendMail(message) {
