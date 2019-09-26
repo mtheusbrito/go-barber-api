0059209521ac58001accf735b3626093eb8f1e20
matheus <matheusbritodasilva@gmail.com>
2019-09-26 15:19:31 -0300

    Tratamento de exceções

--- src/app.js
+++ src/app.js
@@ -1,16 +1,25 @@
 import express from 'express';
+import Youch from 'youch';
 import path from 'path';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors';
 import routes from './routes';
 import './database';
+import sentryConfig from './config/sentry';
 
 class App {
     constructor() {
         this.server = express();
+
+        Sentry.init(sentryConfig);
+
         this.middleweres();
+
         this.routes();
     }
 
     middleweres() {
+        this.server.use(Sentry.Handlers.requestHandler());
         this.server.use(express.json());
         this.server.use(
             '/files',
@@ -20,6 +29,14 @@ class App {
 
     routes() {
         this.server.use(routes);
+        this.server.use(Sentry.Handlers.errorHandler());
+    }
+
+    exceptionHandle() {
+        this.server.use(async (err, req, res, next) => {
+            const errors = await new Youch(err, req).toJSON();
+            return res.status(500).json(errors);
+        });
     }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+    dsn: 'https://dc254a0081564d2e817f218651b6765b@sentry.io/1762844',
+};
